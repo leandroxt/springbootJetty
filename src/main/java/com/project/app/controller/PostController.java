2e2fa69f27b8b6cfb281cb2be7c1e6a093069a88
leandroxt <thiers.leandro@gmail.com>
2016-06-26 16:23:32 -0300

    find by id

--- src/main/java/com/project/app/controller/PostController.java
+++ src/main/java/com/project/app/controller/PostController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by leandro on 12/06/16.
@@ -26,6 +23,11 @@ public class PostController {
         return new ResponseEntity<>(postService.getAll(), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    ResponseEntity<?> getById(@PathVariable String id) {
+        return new ResponseEntity<>(postService.findById(id), HttpStatus.OK);
+    }
+
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json; charset=UTF-8")
     ResponseEntity<?> add(@RequestBody Post post) {
         return new ResponseEntity<>(postService.add(post), HttpStatus.CREATED);
@@ -36,7 +38,8 @@ public class PostController {
         return new ResponseEntity<>(postService.update(post), HttpStatus.OK);
     }
 
-    ResponseEntity<?> delete(@PathVariable String id) {
+    @RequestMapping(method = RequestMethod.DELETE)
+    ResponseEntity<?> delete(@PathVariable("id") String id) {
         Post post = postService.findById(id);
         postService.delete(post);
         return new ResponseEntity<>(post, HttpStatus.OK);
