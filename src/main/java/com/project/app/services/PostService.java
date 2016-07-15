ad43ca932d975f9183223abc202f8ec59a8edf24
leandroxt <thiers.leandro@gmail.com>
2016-07-14 22:27:47 -0300

    removing unused imports

--- src/main/java/com/project/app/services/PostService.java
+++ src/main/java/com/project/app/services/PostService.java
@@ -4,8 +4,6 @@ import com.project.app.domain.Post;
 import com.project.app.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
